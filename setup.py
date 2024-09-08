0a50e7a6852ada2e349cd220e5ff8ab498f3c8be
Jackson Makl <127900729+jacksonlmakl@users.noreply.github.com>
2024-09-08 02:18:48 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='dream_platform_client',
-    version='0.1',
+    version='0.9',
     packages=find_packages(),
     install_requires=[
         'requests',
