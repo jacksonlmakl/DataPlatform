c3f40c757794d2858fc9516bce03333052e55e8b
Jackson Makl <127900729+jacksonlmakl@users.noreply.github.com>
2024-09-08 01:34:51 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from setuptools import setup, find_packages
+import pathlib
+
+# Read the contents of your README file
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='DataPlatform',
@@ -9,9 +14,11 @@ setup(
         'pandas',
     ],
     author='Your Name',
-    author_email='your.email@example.com',
+    author_email='jlm487@georgetown.edu',
     description='A Python client to interact with the DataPlatform API.',
-    url='https://github.com/yourusername/yourrepo',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # Specify Markdown here
+    url='https://github.com/jacksonlmakl/DataPlatform',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
