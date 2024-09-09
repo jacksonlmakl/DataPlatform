80fc933d4380299ac52b9e14e0008758558247ff
Jackson Makl <127900729+jacksonlmakl@users.noreply.github.com>
2024-09-08 22:29:58 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='dataPlatformClient',
-    version='3.0',
+    version='5.0',
     packages=find_packages(),
     install_requires=[
         'requests',
