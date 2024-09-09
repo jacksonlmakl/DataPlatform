37943afe66a49f059f8095b0d94e4a6e66fd799c
Jackson Makl <127900729+jacksonlmakl@users.noreply.github.com>
2024-09-09 13:41:01 -0400

    Update client.py

--- dataPlatformClient/client.py
+++ dataPlatformClient/client.py
@@ -108,19 +108,18 @@ class New:
             'password': password,
             'invite_code': invite_code,
         }
+        self.email=email
+        self.password = password
         # Make the GET request
         response = requests.get(url, params=params)
         # Check if the request was successful
         if response.status_code == 200:
             # Parse the JSON response
             data = response.json()
-            print(data)
-            return Connect(email, password)
+            print("New user added to account")
         else:
-            return response
             print(f"Error: {response.status_code}, {response.text}")
-        
 
-            
-        self.settings={'user_settings':user_settings,'account_settings':account_settings}
-        return {'user_settings':user_settings,'account_settings':account_settings}
+    def client(self):
+        return Connect(self.email, self.password)
+
